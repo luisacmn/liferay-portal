f92568fff1fcac17d932468e5cd35ea758ab3eec
Camilla Almeida <camilla.almeida@liferay.com>
2025-04-25 16:41:13 -0300

    LRSD-9266 [Learn] - The copy button on the lessons code blocks doesn't work

--- workspaces/liferay-learn-workspace/client-extensions/liferay-learn-site-initializer/site-initializer/fragments/group/lms/fragments/lms-lesson-display-page-layout/index.js
+++ workspaces/liferay-learn-workspace/client-extensions/liferay-learn-site-initializer/site-initializer/fragments/group/lms/fragments/lms-lesson-display-page-layout/index.js
@@ -1,22 +1,21 @@
 /**
- * SPDX-FileCopyrightText: (c) 2025 Liferay, Inc. https://liferay.com
+ * SPDX-FileCopyrightText: (c) 2000 Liferay, Inc. https://liferay.com
  * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
  */
-
 const copyToClipboardButtons = document.querySelectorAll(
 	'.copy-to-clipboard-button'
 );
 
 copyToClipboardButtons.forEach((button) => {
 	button.addEventListener('click', () => {
+
 		const codeToolbar = button.closest('.code-toolbar');
 
 		if (codeToolbar) {
-			if (codeToolbar.querySelector('code.language-bash').innerText) {
+			if (codeToolbar != null) {
 				navigator.clipboard
 					.writeText(
-						codeToolbar.querySelector('code.language-bash')
-							.innerText
+						codeToolbar.innerText
 					)
 					.then(() => {
 						button.setAttribute('data-copy-state', 'copy-success');
